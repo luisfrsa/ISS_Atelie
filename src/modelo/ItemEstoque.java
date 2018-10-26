1902f596dacf6619e0d85cd4c57ddd99a202c0b4
williamrodrigues1997 <ra99514@uem.br>
2018-10-26 16:01:08 -0300

    Mapeando classe ItemEstoque para o Banco de Dados

--- src/modelo/ItemEstoque.java
+++ src/modelo/ItemEstoque.java
@@ -1,11 +1,27 @@
 package modelo;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tbl_item_estoque")
 public class ItemEstoque {
 
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
 
+    @OneToOne
+    @JoinColumn(name = "produto_id")
     private Produto produto;
 
+    @Column(name = "quantidade")
     private Integer quantidade;
 
     public ItemEstoque() {
